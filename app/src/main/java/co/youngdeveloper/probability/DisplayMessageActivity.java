6f91654540a95de81eda41261b45f490ba2bd261
Young Developer <youngdeveloper.co@gmail.com>
2017-09-21 17:18:20 +0900

    An application to find permutation of 2 integer numbers is created and programmed.

---
+++ app/src/main/java/co/youngdeveloper/probability/DisplayMessageActivity.java
@@ -0,0 +1,45 @@
+package co.youngdeveloper.probability;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String n_value = intent.getStringExtra(MainActivity.N_MESSAGE);
+        String m_value = intent.getStringExtra(MainActivity.M_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        int n_int = Integer.parseInt(n_value);
+        int m_int = Integer.parseInt(m_value);
+
+        long res = 1;
+
+        if(n_int>=m_int)
+        {
+            res=getFact(n_int)/getFact(n_int-m_int);
+            textView.setText(String.valueOf(res));
+        }
+        else System.out.println("m cannot be greater than n");
+
+    }
+
+    public static long getFact(int n) {
+        long f=1;
+
+        for(int i=n; i>=1; i--)
+        {
+            f*=i;
+        }
+
+        return f;
+    }
+}
---
+++ app/src/main/java/co/youngdeveloper/probability/MainActivity.java
@@ -0,0 +1,32 @@
+package co.youngdeveloper.probability;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String N_MESSAGE = "com.example.myfirstapp.N_MESSAGE";
+    public static final String M_MESSAGE = "com.example.myfirstapp.M_MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText value_of_n = (EditText) findViewById(R.id.editValueOfN);
+        EditText value_of_m = (EditText) findViewById(R.id.editValueOfM);
+        String n_message = value_of_n.getText().toString();
+        String m_message = value_of_m.getText().toString();
+
+        intent.putExtra(N_MESSAGE, n_message);
+        intent.putExtra(M_MESSAGE, m_message);
+        startActivity(intent);
+    }
+
+}
